09f683aba8f75568b47774345842b7618a3b297b
Vincent Matthys <vmatthys@student.42.fr>
2017-12-19 16:11:00 +0100

    Performance boost

--- project2/group/SSC.py
+++ project2/group/SSC.py
@@ -22,35 +22,54 @@ def S_Shrinkage(Matrix,tau):
 
     return shrink_vect(Matrix)
 
+def soft_thresholding(mat, epsilon):
+    """
+    Soft thresholding operator
+    Proximal operator of the L1 norm
+    :param mat: matrix to threshold
+    :param epsilon: value of threshold
+    :return mat: thresholded matrix
+    """
 
-def compute_sparse_C(data,tau,mu2):
+    mat[np.abs(mat) < epsilon] = 0
+    mat[mat > epsilon] -= epsilon
+    mat[mat < -epsilon] += epsilon
+
+    return mat
+
+
+def compute_sparse_C(data,tau,mu2, verbose = False):
     global epsilon
     C = np.zeros((data.shape[1],data.shape[1]))
     lambda2 = np.zeros((data.shape[1],data.shape[1]))
-    C_previous = np.ones((data.shape[1],data.shape[1]))*float('inf')
-    lambda2previous = np.ones((data.shape[1],data.shape[1]))*float('inf')
-    Z_previous= np.ones((data.shape[1],data.shape[1]))*float('inf')
+    C_previous = np.inf * np.ones((data.shape[1],data.shape[1]))
+    lambda2previous = np.inf * np.ones((data.shape[1],data.shape[1]))
+    Z_previous = np.inf * np.ones((data.shape[1],data.shape[1]))
     Z = np.zeros((data.shape[1],data.shape[1]))
 
 
     Z_1 = np.linalg.inv(tau*np.dot(data.transpose(),data)+ mu2*np.eye(data.shape[1]))
 
-    while (np.linalg.norm(C-C_previous)>epsilon or np.linalg.norm(Z-Z_previous)>epsilon  or np.linalg.norm(lambda2-lambda2previous)>epsilon):
+    while (np.linalg.norm(C-C_previous) / (epsilon + np.linalg.norm(C)) >epsilon or np.linalg.norm(Z-Z_previous) / (epsilon + np.linalg.norm(Z))>epsilon):
 
         C_previous = np.array(C)
         Z_previous = np.array(Z)
         lambda2previous = np.array(lambda2)
 
 
-        Z_2 = tau*np.dot(data.transpose(),data) + mu2*(C-lambda2/mu2)
+        Z_2 = tau * np.dot(data.transpose(),data) + mu2*(C-lambda2/mu2)
         Z = np.dot(Z_1,Z_2)
 
-        C = S_Shrinkage(Z+lambda2/mu2,1/mu2)
+        # C = S_Shrinkage(Z+lambda2/mu2,1/mu2)
+        ## %lprun -f  compute_sparse_C T = SSC(motion_data[key]["data"], motion_data[key]["s"].max() + 1, 1e7, 1e3) => 115s 99% S_Shrinkage
+        C = soft_thresholding(Z+lambda2/mu2,1/mu2)
+        ## %lprun -f  compute_sparse_C T = SSC(motion_data[key]["data"], motion_data[key]["s"].max() + 1, 1e7, 1e3) => 7.68323 s 25% S_Shrinkage
         np.fill_diagonal(C,0)
 
         lambda2 = lambda2 + mu2*(Z-C)
 
-        print(np.linalg.norm(C-C_previous),np.linalg.norm(Z-Z_previous),np.linalg.norm(lambda2-lambda2previous))
+        if verbose:
+            print(np.linalg.norm(C-C_previous),np.linalg.norm(Z-Z_previous),np.linalg.norm(lambda2-lambda2previous))
 
 
     return C
